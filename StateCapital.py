9825285ebe560af2c92f7f8bc042ab841e8875cf
Mike <33035299+mlinea01@users.noreply.github.com>
2018-01-24 13:30:18 -0800

    More efficient code
    Changed from a series of if statements, to using an array and a for loop.

--- StateCapital.py
+++ StateCapital.py
@@ -1,200 +1,29 @@
 leaveProgram = 0
 
 while leaveProgram != "q":
-    print("Choose a state to see it's captial.(Case sensitive)")
-    state = input()
+    state_caps = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+                  'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover',
+                  'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+                  'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka',
+                  'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
+                  'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+                  'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City',
+                  'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+                  'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem',
+                  'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
+                  'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Lake City',
+                  'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'Wisconsin': 'Madison',
+                  'Wyoming': 'Cheyenne'
+                  }
 
-    if state == "Alabama":
-        print("The capital of",state,"is Montgomery")
-        leaveProgram = input("Press enter to continue or q to quit")
+    for state in state_caps:
+        state = input("Enter a state to see it's Capital.(Case Sensitive)")
 
-    elif state == "Alaska":
-        print("The capital of",state,"is Juneau")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Arizona":
-        print("The capital of",state,"is Phoenix")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Arkansas":
-        print("The capital of",state,"is Little Rock")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "California":
-        print("The capital of",state,"is Sacramento")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Colorado":
-        print("The capital of",state,"is Denver")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Connecticut":
-        print("The capital of",state,"is Hartford")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Delaware":
-        print("The capital of",state,"is Dover")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Florida":
-        print("The capital of",state,"is Tallahassee")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Georgia":
-        print("The capital of",state,"is Atlanta")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Hawaii":
-        print("The capital of",state,"is Honolulu")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Idaho":
-        print("The capital of",state,"is Boise")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Illinois":
-        print("The capital of",state,"is Springfield")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Indiana":
-        print("The capital of",state,"is Indianapolis")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Iowa":
-        print("The capital of",state,"is Des Moines")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Kansas":
-        print("The capital of",state,"is Topeka")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Kentucky":
-        print("The capital of",state,"is Frankfort")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Louisiana":
-        print("The capital of",state,"is Baton Rouge")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Maine":
-        print("The capital of",state,"is Augusta")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Maryland":
-        print("The capital of",state,"is Annapolis")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Massachusetts":
-        print("The capital of",state,"is Boston")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Michigan":
-        print("The capital of",state,"is Lansing")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Minnesota":
-        print("The capital of",state,"is Saint Paul")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Mississippi":
-        print("The capital of",state,"is Jackson")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Missouri":
-        print("The capital of",state,"is Jefferson City")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Montana":
-        print("The capital of",state,"is Helena")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Nebraska":
-        print("The capital of",state,"is Lincoln")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Nevada":
-        print("The capital of",state,"is Carson City")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "New Hampshire":
-        print("The capital of",state,"is Concord")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "New Jersey":
-        print("The capital of",state,"is Trenton")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "New York":
-        print("The capital of",state,"is Albany")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "North Carolina":
-        print("The capital of",state,"is Raleigh")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "North Dakota":
-        print("The capital of",state,"is Bismarck")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Ohio":
-        print("The capital of",state,"is Columbus")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Oklahoma":
-        print("The capital of",state,"is Oklahoma City")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Oregon":
-        print("The capital of",state,"is Salem")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Pennsylvania":
-        print("The capital of",state,"is Harrisburg")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Rhode Island":
-        print("The capital of",state,"is Providence")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "South Carolina":
-        print("The capital of",state,"is Columbia")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "South Dakota":
-        print("The capital of",state,"is Pierre")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Tennessee":
-        print("The capital of",state,"is Nashville")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Texas":
-        print("The capital of",state,"is Austin")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Utah":
-        print("The capital of",state,"is Salt Lake City")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Vermont":
-        print("The capital of",state,"is Montpelier")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Virginia":
-        print("The capital of",state,"is Richmond")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Washington":
-        print("The capital of",state,"is Olympia")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Wisconsin":
-        print("The capital of",state,"is Madison")
-        leaveProgram = input("press enter to continue or q to quit")
-
-    elif state == "Wyoming":
-        print("The capital of",state,"is Cheyenne")
-        leaveProgram = input("press enter to continue or q to quit")
+    if state in state_caps:
+        print(state_caps[state])
+        break
 
     else:
         print("Could not find what you are looking for! Check your spelling and trying again!")
+
+    leaveProgram = input("Press enter to continue or q to quit")
